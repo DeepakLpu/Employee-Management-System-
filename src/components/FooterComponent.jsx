df4db37903d19d6758a5cafe42bcc6380a77f4e8
Deepak Prajapati <164873531+DeepakLpu@users.noreply.github.com>
2025-04-29 12:58:21 +0530

    Add files via upload

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function FooterComponent() {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>
+                All rights reseverd 2024 by Deepak
+            </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
